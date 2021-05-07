3756d5ea57cdfd6e250b4676ccf5127d898ab501
Hiro Fujita <hiro.fujita@he.kanagawa-it.ac.jp>
2021-05-07 19:13:27 +0900

    Initial Commit

---
+++ ssng2/ssng2App.swift
@@ -0,0 +1,17 @@
+//
+//  ssng2App.swift
+//  ssng2
+//
+//  Created by Hiro Fujita on 2021/05/07.
+//
+
+import SwiftUI
+
+@main
+struct ssng2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
